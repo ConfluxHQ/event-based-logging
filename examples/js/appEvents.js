896fb5c9739f86e8e619e65e8e39877a5e6af04e
Matthew Skelton <matthew@confluxdigital.net>
2021-06-16 21:28:27 +0100

    Added spaces to the AppEvents file for clarity

--- examples/js/appEvents.js
+++ examples/js/appEvents.js
@@ -1,17 +1,27 @@
 const Events = Object.freeze({
     UndefinedError : 'UndefinedError',
+
+    // Database events
     DatabaseConnectionSuccess : 'DatabaseConnectionSuccess',
     DatabaseConnectionFailure : 'DatabaseConnectionFailure',
     DatabaseConnectionTimeout : 'DatabaseConnectionTimeout',
+
+    // Parsing events
     ParseStreamUnexpectedToken : 'ParseStreamUnexpectedToken',
     ParseStreamMissingData : 'ParseStreamMissingData',
     ParseStreamSuccess : 'ParseStreamSuccess',
+    
+    // TOken validation events
     TokenValidationSucceeded : 'TokenValidationSucceeded',
     TokenValidationFailedInvalidParams : 'TokenValidationFailedInvalidParams',
-    TokenValidationFailedInvalidDigest : 'okenValidationFailedInvalidDigest',
+    TokenValidationFailedInvalidDigest : 'TokenValidationFailedInvalidDigest',
     TokenValidationFailedIncorrectSHA : 'TokenValidationFailedIncorrectSHA',
+
+    // Application lifecycle events
     AppStarted : 'AppStarted',
     AppShutdownRequested : 'AppShutdownRequested',
+
+    // Test event
     NoOp : 'NoOp'
   });
 
